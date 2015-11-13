80be8b91e9e00361b893f88ff83eb873416949b6
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2015-11-13 15:27:13 +0900

    GET(一覧取得)とPOST(データ作成)が出来るようになった．

--- edisonHTTPRequestAPI/server.js
+++ edisonHTTPRequestAPI/server.js
@@ -10,10 +10,10 @@ var bodyParser = require('body-parser');
 
 // use MongoDB
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
+mongoose.connect('mongodb://localhost/edisonHTTPRequestAPI'); // connect to our database
 
 // use Model
-var user     = require('./app/models/user');
+var User       = require('./app/models/user');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -33,12 +33,52 @@ router.use(function(req, res, next) {
     next(); // make sure we go to the next routes and don't stop here
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// test route to make sure everything is working (accessed at GET http://localhost:3000/api)
 router.get('/', function(req, res) {
     res.json({ message: 'Shopee!japan' });
 });
 
 // more routes for our API will happen here
+
+// on routes that end in /users
+// ----------------------------------------------------
+router.route('/users')
+
+// create a users (accessed at POST http://localhost:3000/api/users)
+    .post(function(req, res) {
+        
+        var user = new User();      // create a new instance of the User model
+        user.name = req.body.name;  // set the users name (comes from the request)
+        
+        // save the bear and check for errors
+        user.save(function(err) {
+            if (err)
+                res.send(err);
+            res.json({ message: 'User created!' });
+        });
+    })
+
+// get all the bears (accessed at GET http://localhost:8080/api/bears)
+    .get(function(req, res) {
+        User.find(function(err, users) {
+            if (err)
+                res.send(err);
+            res.json(users);
+        });
+    });
+
+// on routes that end in /users/:user_id
+// ----------------------------------------------------
+router.route('/users/:user_id')
+
+// get the user with that id (accessed at GET http://localhost:3000/api/users/:user_id)
+    .get(function(req, res) {
+        User.findById(req.params.user_id, function(err, user) {
+            if (err)
+                res.send(err);
+            res.json(user);
+        });
+    });
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
