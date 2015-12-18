9b42d77a732f85312f4f062f02c6edccadd21062
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2015-12-18 17:47:41 +0900

    ADD:user model

--- server/app/models/user.js
+++ server/app/models/user.js
@@ -5,6 +5,7 @@ var Schema       = mongoose.Schema;
 
 var UserSchema   = new Schema({
     twitter_id : { type: String, required: true, unique: true },
+    room_id    : String,
     longitude  : Number,
     latitude   : Number,
     is_abnormality : Boolean
