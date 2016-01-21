0578722daff01c8d51f4dff6243d3579ec90f97b
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2016-01-22 01:12:16 +0900

    prpr_manの負担が減った

--- server/sql_server.js
+++ server/sql_server.js
@@ -8,7 +8,7 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
-// sequelize prpr_man
+// sequelize
 var Sequelize = require('sequelize');
 
 // use Model
