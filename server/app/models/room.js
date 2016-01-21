601b14e14335e69befffbf1beb9309f47dd45228
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2016-01-22 01:16:08 +0900

    FIX: sample -> asobeat

--- server/app/models/room.js
+++ server/app/models/room.js
@@ -1,7 +1,7 @@
 // app/models/room.js
 
 var Sequelize = require('sequelize');
-var database = new Sequelize('sample','','',{dialect:'sqlite',storage:'./database.db'});
+var database = new Sequelize('asobeat','','',{dialect:'sqlite',storage:'./database.db'});
 
 var Room =  database.define('Room', {
     host_user: {type:Sequelize.STRING,allowNull: false}
--- server/app/models/user.js
+++ server/app/models/user.js
@@ -1,7 +1,7 @@
 // app/models/user.js prpr_man
 
 var Sequelize = require('sequelize');
-var database = new Sequelize('sample','','',{dialect:'sqlite',storage:'./database.db'});
+var database = new Sequelize('asobeat','','',{dialect:'sqlite',storage:'./database.db'});
 
 var User =  database.define('User', {
     twitter_id: {type:Sequelize.STRING,allowNull: false},
