15087426439eea0a2ef5bcfb1f707bf6e0037251
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2016-01-22 01:10:42 +0900

    MOD モデル変更に伴う変更

--- server/create_db.js
+++ server/create_db.js
@@ -2,16 +2,11 @@
 var Sequelize = require('sequelize');
 var database = new Sequelize('sample','','',{dialect:'sqlite',storage:'./database.db'});
 
-var User =  database.define('User', {
-    twitter_id: Sequelize.STRING,
-    room_id: Sequelize.STRING,
-    longitude: Sequelize.INTEGER,
-    latitude: Sequelize.INTEGER,
-    is_abnormality: Sequelize.BOOLEAN
-});
-var Room =  database.define('Room', {
-    host_user: Sequelize.STRING
-});
+var User       = require('./app/models/user');
+var Room       = require('./app/models/room');
+
+User.belongsTo(Room, {foreignKey: 'room_id'});
+Room.hasMany(User , {foreignKey: 'room_id'});
 
 database
     .sync({ force: true });
