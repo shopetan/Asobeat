02abe21004a6794e15386202416d14311cda1f66
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2016-02-01 19:09:26 +0900

    FIX

--- server/app/models/user.js
+++ server/app/models/user.js
@@ -1,4 +1,4 @@
-// app/models/user.js prpr_man
+// app/models/user.js
 
 var Sequelize = require('sequelize');
 var database = new Sequelize('asobeat','','',{dialect:'sqlite',storage:'./database.db'});
